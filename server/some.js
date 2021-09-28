a22c600152cd8dd3612bd2fb4a7201aac16d4d3c
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-09-27 21:22:18 -0500

    class some

---
+++ server/some.js
@@ -0,0 +1,66 @@
+const numbers = [1, 2, 3, 4];
+
+const res = numbers.some(item => item % 2 === 0);
+console.log(res);
+
+const orders = [
+  {
+    customerName: 'Nicolas',
+    total: 60,
+    delivered: true
+  },
+  {
+    customerName: 'Zulema',
+    total: 120,
+    delivered: false
+  },
+  {
+    customerName: 'Santiago',
+    total: 180,
+    delivered: true
+  },
+  {
+    customerName: 'Valentina',
+    total: 1000,
+    delivered: true
+  }
+];
+
+const res2 = orders.some(item => item.delivered);
+console.log(res2);
+
+const dates = [
+  {
+    startDate: new Date(2021, 1, 1, 10),
+    endDate: new Date(2021, 1, 1, 11),
+    title: 'Cita de trabajo'
+  },
+  {
+    startDate: new Date(2021, 1, 1, 15),
+    endDate: new Date(2021, 1, 1, 15, 30),
+    title: 'Cita con mi jefe'
+  },
+  {
+    startDate: new Date(2021, 1, 1, 20),
+    endDate: new Date(2021, 1, 1, 21),
+    title: 'Cena'
+  }
+];
+
+const newAppointment = {
+  startDate: new Date(2021, 1, 1, 8),
+  endDate: new Date(2021, 1, 1, 9, 30)
+};
+
+const areIntervalsOverlapping = require('date-fns/areIntervalsOverlapping');
+
+const isOverlap = newDate => {
+  return dates.some(date => {
+    return areIntervalsOverlapping(
+      { start: date.startDate, end: date.endDate },
+      { start: newDate.startDate, end: newDate.endDate }
+    );
+  });
+};
+
+console.log('Is Overlap', isOverlap(newAppointment));
