be261227effe8442d375d159b3d62121af050943
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-09-28 11:07:35 -0500

    concat

--- server/concat.js
+++ server/concat.js
@@ -1,3 +1,6 @@
+const elements = [1, 1, 2, 2];
+const othersElements = [3, 3, 4, 4];
+
 const newArray = [...elements];
 for (let index = 0; index < othersElements.length; index++) {
   const element = othersElements[index];
@@ -11,5 +14,6 @@ console.log('for', newArray);
 console.log('concat', rta);
 console.log('...', rta2);
 console.log('rta3', rta3);
+
 elements.push(...othersElements);
 console.log('elements', elements);
