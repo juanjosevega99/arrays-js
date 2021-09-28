7435ed4fcf40c9d40c3f98a214e8e483f8a78a07
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-09-27 20:47:44 -0500

    reduce reloaded challenge

--- server/reduce-reloaded.js
+++ server/reduce-reloaded.js
@@ -10,3 +10,28 @@ const response = items.reduce((obj, item) => {
 }, {});
 
 console.log(response);
+
+const list = [];
+
+for (let index = 0; index < 30; index++) {
+  list.push(Math.floor(Math.random() * 11));
+}
+
+console.log(list)
+
+const result = list.reduce(
+  (obj, value) => {
+    if (value < 5) obj['1-4'] += 1;
+    else if (value < 9) obj['5-8'] += 1;
+    else obj['9-10'] += 1;
+
+    return obj;
+  },
+  {
+    '1-4': 0,
+    '5-8': 0,
+    '9-10': 0
+  }
+);
+
+console.log(result)
