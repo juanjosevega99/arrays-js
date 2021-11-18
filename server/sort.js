b5756a8b3a68ff4cbd39fecd3a2fbb5c10c86412
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-11-17 21:49:19 -0500

    add sort example

---
+++ server/sort.js
@@ -0,0 +1,44 @@
+const months = ['March', 'Jan', 'Feb', 'Dec'];
+months.sort();
+console.log(months);
+
+const numbers = [1, 30, 4, 21, 100000];
+numbers.sort((a, b) => b - a);
+console.log(numbers);
+
+const words = [
+  'réservé',
+  'premier',
+  'communiqué',
+  'café',
+  'adieu',
+  'éclair',
+  'banana'
+];
+words.sort();
+console.log('🚀 ~ file: sort.js ~ line 22 ~ words', words);
+
+const orders = [
+  {
+    customerName: 'Nicolas',
+    total: 600,
+    delivered: true
+  },
+  {
+    customerName: 'Zulema',
+    total: 120,
+    delivered: false
+  },
+  {
+    customerName: 'Santiago',
+    total: 1840,
+    delivered: true
+  },
+  {
+    customerName: 'Valentina',
+    total: 240,
+    delivered: true
+  }
+];
+orders.sort((a, b) => b.total - a.total);
+console.log('🚀 ~ file: sort.js ~ line 37 ~ orders', orders);
